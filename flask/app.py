f90bab4aaac971c85006f1cfe06c15cb7fbf7f91
vuthehung <hungvt280102@gmail.com>
2025-06-07 12:08:45 +0700

    create flask api

---
+++ flask/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/api/get_data', methods=['GET'])
+def get_data():
+    try:
+        year = request.args.get('year')
+        month = request.args.get('month')
+        day = request.args.get('day')
+        offset = int(request.args.get('offset'))
+        limit = int(request.args.get('limit'))
+
+        df = pd.read_csv(f'./data/stock_transactions_{year}_{month}_{day}.csv')
+        cnt = len(df)
+
+        if offset + limit >= cnt:
+            result = df.iloc[offset:]
+            return jsonify({'status': 'complete', 'data': result.to_dict(orient='records')})
+
+        result = df.iloc[offset:offset+limit]
+        return jsonify({'status': 'success', 'data': result.to_dict(orient='records')})
+    except Exception as e:
+        print(e)
+        return jsonify({'status': 'error', 'data': 'error'})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)

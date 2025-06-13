6d4a6306dfaf140069ed6fab74073e2753839b4c
vuthehung <hungvt280102@gmail.com>
2025-06-13 22:52:58 +0700

    mini fix

--- airflow/dags/batch_dag.py
+++ airflow/dags/batch_dag.py
@@ -203,12 +203,12 @@ with DAG(
         trino_conn_id="trino_conn",
         sql=SQL_INSERT_daily_exchange_summary,
     )
-    pre_processing_job = SparkSubmitOperator(
-        task_id="pre-processing",
+    clean_data_job = SparkSubmitOperator(
+        task_id="clean_data",
         application="/opt/airflow/code/clean_data.py",
         conn_id="spark_conn",
         verbose=True,
         packages="org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.4.2,org.apache.iceberg:iceberg-aws-bundle:1.4.2,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.1026"
     )
 
-    pre_processing_job >> create_schema_task >> [[create_daily_market_summary_task >> insert_daily_market_summary_task], [create_daily_stock_summary_task >> insert_daily_stock_summary_task], [create_daily_order_type_summary_task >> insert_daily_order_type_summary_task], [create_daily_exchange_summary_task >> insert_daily_exchange_summary_task]]
+    clean_data_job >> create_schema_task >> [[create_daily_market_summary_task >> insert_daily_market_summary_task], [create_daily_stock_summary_task >> insert_daily_stock_summary_task], [create_daily_order_type_summary_task >> insert_daily_order_type_summary_task], [create_daily_exchange_summary_task >> insert_daily_exchange_summary_task]]

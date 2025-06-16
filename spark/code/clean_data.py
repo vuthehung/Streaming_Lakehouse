ad2553c25cd4fe62ea1edb6ce6cc8fcced9b48fb
vuthehung <hungvt280102@gmail.com>
2025-06-16 15:48:08 +0700

    update

--- spark/code/clean_data.py
+++ spark/code/clean_data.py
@@ -35,8 +35,18 @@ def run_bronze_to_silver_job(bronze_table, silver_table):
         return
 
     # Xử lý NULL
-    cleaned_df = filtered_df.na.drop(subset=["price", "quantity", "order_type", "exchange"])
-    print(f"Số bản ghi sau khi loại bỏ NULL: {cleaned_df.count()}.")
+    filtered_df.createOrReplaceTempView("daily_transactions_to_clean")
+
+    sql_clean_query = """
+                      SELECT *
+                      FROM daily_transactions_to_clean
+                      WHERE (price IS NOT NULL AND NOT isnan(price))
+                        AND (quantity IS NOT NULL AND NOT isnan(quantity))
+                        AND (order_type IS NOT NULL AND order_type != '"NaN"')
+                        AND (exchange IS NOT NULL AND exchange != '"NaN"') \
+                      """
+    cleaned_df = spark.sql(sql_clean_query)
+    print(f"Số bản ghi sau khi loại bỏ NULL và các giá trị không hợp lệ: {cleaned_df.count()}.")
 
     # Loại bỏ TRÙNG LẶP (Deduplication)
     window_spec = Window.partitionBy("transaction_id").orderBy(col("ts").desc())
